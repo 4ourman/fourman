68341c0c4e5d9e2889a7e319b5602b60be6090a3
DESKTOP-E9CUUFE\804-19 <cacao3216@gmail.com>
2024-03-04 15:51:11 +0900

    메인페이지,로그인창,회원가입 화면 기능없이 틀만 구현

--- src/main/java/org/fourman/sojuproject/controller/PostController.java
+++ src/main/java/org/fourman/sojuproject/controller/PostController.java
@@ -1,4 +1,24 @@
 package org.fourman.sojuproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/main")
+    public String mainpage(){
+        return "/comment/main";
+    }
+
+    @GetMapping("/joinmember")
+    public String joinpage(){
+        return "/comment/joinmember";
+    }
+
+    @GetMapping("/createpost")
+    public String createpost(){
+        return "/comment/createpost";
+    }
+
 }
