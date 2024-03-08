0616659f0e53a4810ab8aa201cd6a7c2285a486e
이은재 <>
2024-03-08 12:14:10 +0900

    [Feat + View] update.html 페이지 생성 및 기능 Combine

--- src/main/java/org/fourman/sojuproject/controller/PostController.java
+++ src/main/java/org/fourman/sojuproject/controller/PostController.java
@@ -1,11 +1,10 @@
 package org.fourman.sojuproject.controller;
 
-
 import lombok.extern.log4j.Log4j2;
 import org.fourman.sojuproject.domain.entity.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.fourman.sojuproject.domain.dto.post.*;
@@ -16,9 +15,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/posts")
@@ -30,25 +26,20 @@ public class PostController {
 
     @GetMapping("/main")
     public String main(Model model, @PageableDefault(page = 0, size = 10, sort = "postId",
-            direction = Sort.Direction.DESC) Pageable pageable, String searchKeyword) { //검색 키워드를 searchKeyword로 전달 받음
+            direction = Sort.Direction.DESC) Pageable pageable, String searchKeyword) {
 
         Page<Post> readposts;
 
-        if(searchKeyword == null) { //검색할 키워드가 들어오지 않은 경우 전체 리스트 출력
-            // 제목과 작성자가 바뀌어서 출력되는 상태
+        if(searchKeyword == null) {
             readposts = postService.readSearch(pageable);
         } else {
-            //검색할 키워드가 들어온 경우 검색 기능이 포함된 리스트 반환
-            // 제목과 작성자가 정상적으로 출력되는 상태
             log.info("✌ searchKeyword : {}", searchKeyword);
             readposts = postService.searchPost(searchKeyword, pageable);
         }
 
-        int nowPage = readposts.getPageable().getPageNumber() + 1; //pageable이 가지고 있는 페이지는 0부터 시작이라 1을 더함
-
-        int startPage = Math.max(nowPage -4, 1); //1보다 작은 경우는 1을 반환
-
-        int endPage = Math.min(nowPage + 5, readposts.getTotalPages()); //전체 페이지보다 많은 경우는 전체 페이지를 반환
+        int nowPage = readposts.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage -4, 1);
+        int endPage = Math.min(nowPage + 5, readposts.getTotalPages());
 
         model.addAttribute("readposts", readposts);
         model.addAttribute("nowPage",nowPage);
@@ -97,7 +88,7 @@ public class PostController {
         return "/comment/update";
     }
 
-    @PutMapping("/{postId}")
+    @PutMapping("/update/{postId}")
     @Operation(summary = "게시글 수정", description = "postId와 request로 게시글 수정")
     public ResponseEntity<UpdatePostResponseDTO> postUpdate(@PathVariable Long postId, @RequestBody UpdatePostRequestDTO requestDTO) {
 
@@ -106,6 +97,7 @@ public class PostController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 
     }
+
 
     @DeleteMapping("/{postId}")
     @Operation(summary = "게시글 삭제", description = "postId로 게시글 삭제")
