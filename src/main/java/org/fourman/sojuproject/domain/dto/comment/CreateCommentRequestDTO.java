7f464e606938c655ba834861fcdb7b1b60726136
DESKTOP-68QM4EC\hi <sy99639@naver.com>
2024-03-12 11:56:36 +0900

    [Feat] 댓글 구현

--- src/main/java/org/fourman/sojuproject/domain/dto/comment/CreateCommentRequestDTO.java
+++ src/main/java/org/fourman/sojuproject/domain/dto/comment/CreateCommentRequestDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -15,11 +16,23 @@ public class CreateCommentRequestDTO {
     private String u_nick_name;
     @NotNull
     private String c_content;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime regDate;
-    private LocalDateTime modDate;
+    private LocalDateTime modDate; // 수정 시간 필드 추가
 
+    public LocalDateTime getRegDate() {
+        return regDate;
+    }
 
+    public void setRegDate(LocalDateTime regDate) {
+        this.regDate = regDate;
+    }
 
+    public LocalDateTime getModDate() {
+        return modDate;
+    }
 
-
+    public void setModDate(LocalDateTime modDate) {
+        this.modDate = modDate;
+    }
 }
